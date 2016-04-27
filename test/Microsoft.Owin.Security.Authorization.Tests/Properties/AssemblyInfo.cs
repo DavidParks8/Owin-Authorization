ec903ee157f68fcd5e3d126bb94d70e048349897
David <daparks923@yahoo.com>
2016-04-27 01:35:59 -0500

    Added a testing project and took care of some warnings

--- test/Microsoft.Owin.Security.Authorization.Tests/Properties/AssemblyInfo.cs
+++ test/Microsoft.Owin.Security.Authorization.Tests/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Resources;
-using System;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -7,15 +6,14 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Microsoft.Owin.Security.Authorization")]
+[assembly: AssemblyTitle("Microsoft.Owin.Security.Authorization.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Microsoft.Owin.Security.Authorization")]
+[assembly: AssemblyProduct("Microsoft.Owin.Security.Authorization.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: CLSCompliant(true)]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -23,7 +21,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a74dc534-bd34-419b-9fd4-dabcbae53a46")]
+[assembly: Guid("25df3abb-1e2e-4f7b-a517-98fe6d47412a")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -35,7 +33,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
---
+++ test/Microsoft.Owin.Security.Authorization.Tests/ResourceAuthorizationAttributeTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Security.Claims;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.Owin.Security.Authorization
+{
+    [TestClass, ExcludeFromCodeCoverage]
+    public class ResourceAuthorizationAttributeTest
+    {
+        [TestMethod, ExpectedException(typeof(ArgumentNullException))]
+        public void ResourceAuthorizeShouldThrowWhenPassedNull()
+        {
+            var attribute = new ResourceAuthorizeAttribute();
+            attribute.OnAuthorization(null);
+        }
+    }
+}
