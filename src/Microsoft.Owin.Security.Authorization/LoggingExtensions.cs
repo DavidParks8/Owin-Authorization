3671613a0ada20b4303b3a80891d150c12cb18da
David <daparks923@yahoo.com>
2016-04-28 21:16:12 -0500

    Added basic log messages

--- src/Microsoft.Owin.Security.Authorization/LoggingExtensions.cs
+++ src/Microsoft.Owin.Security.Authorization/LoggingExtensions.cs
@@ -1,36 +1,31 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using Microsoft.Owin.Logging;
 
 namespace Microsoft.Owin.Security.Authorization
 {
     internal static class LoggingExtensions
     {
-        //private static Action<ILogger, string, Exception> _userAuthorizationFailed;
-        //private static Action<ILogger, string, Exception> _userAuthorizationSucceeded;
-
-        static LoggingExtensions()
-        {
-            //todo: add logging actions
-            //_userAuthorizationSucceeded = LoggerMessage.Define<string>(
-            //    eventId: 1,
-            //    logLevel: .Information,
-            //    formatString: "Authorization was successful for user: {UserName}.");
-            //_userAuthorizationFailed = LoggerMessage.Define<string>(
-            //    eventId: 2,
-            //    logLevel: LogLevel.Information,
-            //    formatString: "Authorization failed for user: {UserName}.");
-        }
-
         public static void UserAuthorizationSucceeded(this ILogger logger, string userName)
         {
-            //_userAuthorizationSucceeded(logger, userName, null);
+            if (logger == null)
+            {
+                throw new ArgumentNullException(nameof(logger));
+            }
+
+            logger.WriteInformation($"Authorization was successful for user: {userName}");
         }
 
         public static void UserAuthorizationFailed(this ILogger logger, string userName)
         {
-            //_userAuthorizationFailed(logger, userName, null);
+            if (logger == null)
+            {
+                throw new ArgumentNullException(nameof(logger));
+            }
+
+            logger.WriteInformation($"Authorization failed for user: {userName}");
         }
     }
 }
