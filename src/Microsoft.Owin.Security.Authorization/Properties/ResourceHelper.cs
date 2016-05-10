cde874dffb52dde94bf351f9d03afa6c4b54d39e
David <daparks923@yahoo.com>
2016-05-09 20:32:14 -0500

    Added resource helper test

--- src/Microsoft.Owin.Security.Authorization/Properties/ResourceHelper.cs
+++ src/Microsoft.Owin.Security.Authorization/Properties/ResourceHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 
 namespace Microsoft.Owin.Security.Authorization.Properties
---
+++ test/Microsoft.Owin.Security.Authorization.Tests/ResourceHelperTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.Owin.Security.Authorization
+{
+    [TestClass, ExcludeFromCodeCoverage]
+    public class ResourceHelperTests
+    {
+        [TestMethod, UnitTest]
+        public void EnsureAuthorizationPolicyNotFoundIsFormatted()
+        {
+            const string toReplace = "test string";
+            var expected = string.Format(CultureInfo.CurrentCulture, Properties.Resources.Exception_AuthorizationPolicyNotFound, toReplace);
+            var actual = Properties.ResourceHelper.FormatException_AuthorizationPolicyNotFound(toReplace);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
