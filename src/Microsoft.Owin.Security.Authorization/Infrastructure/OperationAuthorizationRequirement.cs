b5e8079e262ca29134da3af91d3b2b6015242495
David <daparks923@yahoo.com>
2016-05-21 16:46:57 -0500

    No testing is necessary for a single simple property.  Fixes #24

--- src/Microsoft.Owin.Security.Authorization/Infrastructure/OperationAuthorizationRequirement.cs
+++ src/Microsoft.Owin.Security.Authorization/Infrastructure/OperationAuthorizationRequirement.cs
@@ -1,10 +1,13 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System.Diagnostics.CodeAnalysis;
+
 namespace Microsoft.Owin.Security.Authorization.Infrastructure
 {
     public class OperationAuthorizationRequirement : IAuthorizationRequirement
     {
+        [ExcludeFromCodeCoverage]
         public string Name { get; set; }
     }
 }
