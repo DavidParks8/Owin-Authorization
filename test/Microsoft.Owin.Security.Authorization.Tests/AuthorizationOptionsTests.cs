fc3788a6710ad4dc2606b9c520fab0fa2b22975d
David <daparks923@yahoo.com>
2016-05-30 22:21:41 -0500

    Added a test for an edge case

--- test/Microsoft.Owin.Security.Authorization.Tests/AuthorizationOptionsTests.cs
+++ test/Microsoft.Owin.Security.Authorization.Tests/AuthorizationOptionsTests.cs
@@ -22,6 +22,20 @@ namespace Microsoft.Owin.Security.Authorization
         }
 
         [TestMethod, UnitTest]
+        public void AuthorizeDependenciesShouldNotBeNullByDefault()
+        {
+            var options = NewOptions();
+            Assert.IsNotNull(options.Dependencies);
+        }
+
+        [TestMethod, UnitTest]
+        public void AuthorizeDependenciesShouldHaveALoggerFactoryInstance()
+        {
+            var options = NewOptions();
+            Assert.IsNotNull(options.Dependencies.LoggerFactory);
+        }
+
+        [TestMethod, UnitTest]
         public void DefaultPolicyShouldRequireAuthenticatedUser()
         {
             var options = NewOptions();
