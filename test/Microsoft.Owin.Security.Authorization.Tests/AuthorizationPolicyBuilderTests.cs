599d817fbce869fc6ce8e42e22415263386d5183
David <daparks923@yahoo.com>
2016-06-02 23:54:38 -0500

    More tests for #15

--- test/Microsoft.Owin.Security.Authorization.Tests/AuthorizationPolicyBuilderTests.cs
+++ test/Microsoft.Owin.Security.Authorization.Tests/AuthorizationPolicyBuilderTests.cs
@@ -29,13 +29,19 @@ namespace Microsoft.Owin.Security.Authorization
             };
 
             var builder = new AuthorizationPolicyBuilder();
-            Assert.AreEqual(0, builder.AuthenticationSchemes.Count);
             builder.AddAuthenticationSchemes(schemes);
             Assert.AreEqual(schemes.Length, builder.AuthenticationSchemes.Count);
             for (var i = 0; i < schemes.Length; i++)
             {
                 Assert.AreEqual(schemes[i], builder.AuthenticationSchemes[i]);
             }
+        }
+
+        [TestMethod, UnitTest]
+        public void AuthenticationSchemesShouldBeEmptyOnConstruction()
+        {
+            var builder = new AuthorizationPolicyBuilder();
+            Assert.AreEqual(0, builder.AuthenticationSchemes.Count);
         }
 
         [TestMethod, UnitTest, ExpectedException(typeof(ArgumentNullException))]
@@ -57,13 +63,19 @@ namespace Microsoft.Owin.Security.Authorization
             };
 
             var builder = new AuthorizationPolicyBuilder();
-            Assert.AreEqual(0, builder.Requirements.Count);
             builder.AddRequirements(requirements);
             Assert.AreEqual(requirements.Length, builder.Requirements.Count);
             for (var i = 0; i < requirements.Length; i++)
             {
                 Assert.AreEqual(requirements[i], builder.Requirements[i]);
             }
+        }
+
+        [TestMethod, UnitTest]
+        public void RequirementsShouldBeEmptyOnConstruction()
+        {
+            var builder = new AuthorizationPolicyBuilder();
+            Assert.AreEqual(0, builder.Requirements.Count);
         }
 
         [TestMethod, UnitTest]
@@ -92,6 +104,73 @@ namespace Microsoft.Owin.Security.Authorization
         {
             var property = typeof(AuthorizationPolicyBuilder).GetProperty(nameof(AuthorizationPolicyBuilder.Requirements));
             Assert.IsFalse(property.CanWrite, "property.CanWrite");
+        }
+
+        [TestMethod, UnitTest, ExpectedException(typeof(ArgumentNullException))]
+        public void CombineShouldThrowWhenPolicyIsNull()
+        {
+            var builder = new AuthorizationPolicyBuilder();
+            builder.Combine(null);
+        }
+
+        [TestMethod, UnitTest]
+        public void ConstructorShouldCombinePolicy()
+        {
+            AssertCombineSuccess(policy => new AuthorizationPolicyBuilder(policy));
+        }
+
+        [TestMethod, UnitTest]
+        public void CombineShouldSucceed()
+        {
+            AssertCombineSuccess(policy =>
+            {
+                var builder = new AuthorizationPolicyBuilder();
+                builder.Combine(policy);
+                return builder;
+            });
+        }
+
+        private static void AssertCombineSuccess(Func<AuthorizationPolicy, AuthorizationPolicyBuilder> testFunc)
+        {
+            var requirements = new IAuthorizationRequirement[]
+            {
+                new DenyAnonymousAuthorizationRequirement()
+            };
+            var schemes = new[] { "test" };
+
+            var policy = new AuthorizationPolicy(requirements, schemes);
+            var builder = testFunc(policy);
+
+
+            Assert.AreEqual(requirements.Length, builder.Requirements.Count);
+            Assert.AreSame(requirements[0], builder.Requirements[0]);
+            Assert.AreEqual(schemes.Length, builder.AuthenticationSchemes.Count);
+            Assert.AreEqual(schemes[0], builder.AuthenticationSchemes[0]);
+        }
+
+        [TestMethod, UnitTest]
+        public void RequireAuthenticatedUserShouldAddDenyRequirement()
+        {
+            var builder = new AuthorizationPolicyBuilder();
+            builder.RequireAuthenticatedUser();
+            Assert.IsInstanceOfType(builder.Requirements[0], typeof(DenyAnonymousAuthorizationRequirement));
+        }
+
+        [TestMethod, UnitTest, ExpectedException(typeof(ArgumentNullException))]
+        public void RequireUserNameShouldThrowWhenUserNameIsNull()
+        {
+            var builder = new AuthorizationPolicyBuilder();
+            builder.RequireUserName(null);
+        }
+
+        [TestMethod, UnitTest]
+        public void RequireUserNameShouldAddNameRequirement()
+        {
+            const string testName = "test";
+            var builder = new AuthorizationPolicyBuilder();
+            builder.RequireUserName(testName);
+            Assert.IsInstanceOfType(builder.Requirements[0], typeof(NameAuthorizationRequirement));
+            Assert.AreEqual(testName, ((NameAuthorizationRequirement)builder.Requirements[0]).RequiredName);
         }
     }
 }
