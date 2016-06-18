0109db828e1532be1d09649f59ae6d27afbffbad
David <daparks923@yahoo.com>
2016-06-18 14:09:57 -0500

    Moved the helper to its own file

--- src/Microsoft.Owin.Security.Authorization/AuthorizationHelper.cs
+++ src/Microsoft.Owin.Security.Authorization/AuthorizationHelper.cs
@@ -73,36 +73,4 @@ namespace Microsoft.Owin.Security.Authorization
             return helper.AuthorizationOptions;
         }
     }
-
-    /// <summary> 
-    /// Extracts authorization objects from the <see cref="IOwinContext"/> environment. 
-    /// </summary> 
-    public class AuthorizationDependencyHelper
-    { 
-         public AuthorizationOptions AuthorizationOptions { get; } 
- 
-
-         public AuthorizationDependencyHelper(IOwinContext context)
-         { 
-             if (context == null) 
-             { 
-                 throw new ArgumentNullException(nameof(context)); 
-             } 
-             if (context.Environment == null) 
-             { 
-                 throw new ArgumentNullException(nameof(context), Resources.ErrorTheOwinEnvironmentDictionaryWasNull); 
-             } 
- 
-
-             object environmentService; 
-             if (context.Environment.TryGetValue(ResourceAuthorizationMiddleware.ServiceKey, out environmentService)) 
-             { 
-                 AuthorizationOptions = (AuthorizationOptions)environmentService; 
-             } 
-             else 
-             { 
-                 throw new InvalidOperationException(Resources.Exception_PleaseSetupOwinResourceAuthorizationInYourStartupFile); 
-             } 
-         } 
-     } 
 }
