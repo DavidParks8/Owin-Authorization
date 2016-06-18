f42dd26cc1eea99bfb0761398b0303653a1d050f
David <daparks923@yahoo.com>
2016-06-18 15:19:30 -0500

    Added a middleware test

--- test/Microsoft.Owin.Security.Authorization.Tests/ResourceAuthorizationMiddlewareTests.cs
+++ test/Microsoft.Owin.Security.Authorization.Tests/ResourceAuthorizationMiddlewareTests.cs
@@ -11,7 +11,7 @@ namespace Microsoft.Owin.Security.Authorization
     {
         [SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic", Justification = Justifications.MustBeInstanceMethod)]
         [TestMethod, UnitTest]
-        public async Task NextShouldBeInvokedWithOptionsInEnvironment()
+        public async Task OptionsShouldBeSetInEnvironment()
         {
             var options = new AuthorizationOptions();
             var middleware = new ResourceAuthorizationMiddleware(null, options);
@@ -21,6 +21,22 @@ namespace Microsoft.Owin.Security.Authorization
             await middleware.Invoke(mockContext.Object);
 
             mockContext.Verify(x => x.Set(ResourceAuthorizationMiddleware.ServiceKey, options), Times.Once);
+        }
+
+        [SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic", Justification = Justifications.MustBeInstanceMethod)]
+        [TestMethod, UnitTest]
+        public async Task NextShouldBeInvoked()
+        {
+            var next = Repository.Create<OwinMiddleware>(null);
+            next.Setup(x => x.Invoke(It.IsAny<IOwinContext>())).Returns(Task.FromResult(0));
+            var middleware = new ResourceAuthorizationMiddleware(next.Object, new AuthorizationOptions());
+
+            var mockContext = Repository.Create<IOwinContext>();
+            mockContext.Setup(x => x.Set(It.IsAny<string>(), It.IsAny<AuthorizationOptions>())).Returns((IOwinContext)null);
+
+            await middleware.Invoke(mockContext.Object);
+
+            next.Verify(x => x.Invoke(mockContext.Object), Times.Once);
         }
     }
 }
