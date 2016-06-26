b4537eb7fbc0e56cfd58058ca6e732c937c9e14b
David Parks <DavidParks8@outlook.com>
2016-06-26 14:28:36 -0500

    Made tests work to fix #42.  Code coverage is at 100%!

--- test/Microsoft.Owin.Security.Authorization.Mvc.Tests/ResourceAuthorizeAttributeTests.cs
+++ test/Microsoft.Owin.Security.Authorization.Mvc.Tests/ResourceAuthorizeAttributeTests.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Diagnostics.CodeAnalysis;
 using System.Security.Claims;
 using System.Web;
 using System.Web.Mvc;
 using Microsoft.Owin.Security.Authorization.TestTools;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace Microsoft.Owin.Security.Authorization.Mvc
 {
@@ -20,8 +22,48 @@ namespace Microsoft.Owin.Security.Authorization.Mvc
             attribute.OnAuthorization(null);
         }
 
+        private sealed class TestAuthorize : ResourceAuthorizeAttribute
+        {
+            public void PassNullToAuthorizeCore()
+            {
+                AuthorizeCore(null);
+            }
+        }
+
+        [TestMethod, UnitTest, ExpectedException(typeof(ArgumentNullException))]
+        public void AuthorizeCoreShouldThrowWhenHttpContextBaseIsNull()
+        {
+            new TestAuthorize().PassNullToAuthorizeCore();
+        }
+
         [TestMethod, UnitTest]
         public void UserShouldNotBeAuthorized()
+        {
+            var options = new AuthorizationOptions();
+            var controller = Repository.Create<ControllerBase>();
+            controller.As<IAuthorizationController>().Setup(x => x.AuthorizationOptions).Returns(options);
+
+            var authorizationContext = new System.Web.Mvc.AuthorizationContext
+            {
+                HttpContext = SetupHttpContextBase().Object,
+                ActionDescriptor = SetupAllowAnonymous(false).Object,
+                Controller = controller.Object
+            };
+
+            var attribute = new ResourceAuthorizeAttribute();
+            attribute.OnAuthorization(authorizationContext);
+            Assert.IsInstanceOfType(authorizationContext.Result, typeof(HttpUnauthorizedResult));
+        }
+
+        private Mock<ActionDescriptor> SetupAllowAnonymous(bool attributePresent)
+        {
+            var action = Repository.Create<ActionDescriptor>();
+            action.Setup(x => x.IsDefined(It.IsAny<Type>(), It.IsAny<bool>())).Returns(attributePresent);
+            action.Setup(x => x.ControllerDescriptor.IsDefined(It.IsAny<Type>(), It.IsAny<bool>())).Returns(attributePresent);
+            return action;
+        }
+
+        private Mock<HttpContextBase> SetupHttpContextBase()
         {
             var user = new ClaimsPrincipal();
             var items = new Hashtable { { "owin.Environment", new Dictionary<string, object>() } };
@@ -29,17 +71,7 @@ namespace Microsoft.Owin.Security.Authorization.Mvc
             var httpContextBase = Repository.Create<HttpContextBase>();
             httpContextBase.Setup(x => x.User).Returns(user);
             httpContextBase.Setup(x => x.Items).Returns(items);
-            var authorizationContext = new System.Web.Mvc.AuthorizationContext();
-            authorizationContext.HttpContext = httpContextBase.Object;
-
-            var options = new AuthorizationOptions();
-            var controller = Repository.Create<ControllerBase>();
-            controller.As<IAuthorizationController>().Setup(x => x.AuthorizationOptions).Returns(options);
-            authorizationContext.Controller = controller.Object;
-
-            var attribute = new ResourceAuthorizeAttribute();
-            attribute.OnAuthorization(authorizationContext);
-            Assert.IsInstanceOfType(authorizationContext.Result, typeof(HttpUnauthorizedResult));
+            return httpContextBase;
         }
 
         [TestMethod, UnitTest]
@@ -66,8 +98,8 @@ namespace Microsoft.Owin.Security.Authorization.Mvc
             var property = typeof(ResourceAuthorizeAttribute).GetProperty(propertyName);
             Assert.AreEqual(typeof(string), property.PropertyType);
             var attribute = new ResourceAuthorizeAttribute();
-            var initialPropertyValue = property.GetValue(attribute);
-            Assert.IsNull(initialPropertyValue);
+            var initialPropertyValue = (string)property.GetValue(attribute);
+            Assert.IsTrue(string.IsNullOrWhiteSpace(initialPropertyValue));
             property.SetValue(attribute, test);
             var newPropertyValue = property.GetValue(attribute);
             Assert.AreEqual(test, newPropertyValue);
