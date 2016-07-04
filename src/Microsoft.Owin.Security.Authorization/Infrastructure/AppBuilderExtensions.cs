b981081c7c779d937e0f42c9af6448f46b1a849c
David Parks <DavidParks8@outlook.com>
2016-07-04 11:16:23 -0500

    Added AppBuilderExtensions comments for #46

--- src/Microsoft.Owin.Security.Authorization/Infrastructure/AppBuilderExtensions.cs
+++ src/Microsoft.Owin.Security.Authorization/Infrastructure/AppBuilderExtensions.cs
@@ -3,8 +3,16 @@ using Owin;
 
 namespace Microsoft.Owin.Security.Authorization.Infrastructure
 {
+    /// <summary>
+    /// Extension methods for setting up authorization services in an <see cref="IAppBuilder" />.
+    /// </summary>
     public static class AppBuilderExtensions
     {
+        /// <summary>
+        /// Adds authorization services to the specified <see cref="IAppBuilder" />. 
+        /// </summary>
+        /// <param name="app">The <see cref="IAppBuilder" /> to add services to.</param>
+        /// <returns>The <see cref="IAppBuilder"/> so that additional calls can be chained.</returns>
         public static IAppBuilder UseAuthorization(this IAppBuilder app)
         {
             if (app == null)
@@ -15,6 +23,12 @@ namespace Microsoft.Owin.Security.Authorization.Infrastructure
             return UseAuthorization(app, new AuthorizationOptions());
         }
 
+        /// <summary>
+        /// Adds authorization services to the specified <see cref="IAppBuilder" />. 
+        /// </summary>
+        /// <param name="app">The <see cref="IAppBuilder" /> to add services to.</param>
+        /// <param name="options">The <see cref="AuthorizationOptions"/> to configure the <paramref name="app"/> with.</param>
+        /// <returns>The <see cref="IAppBuilder"/> so that additional calls can be chained.</returns>
         public static IAppBuilder UseAuthorization(this IAppBuilder app, AuthorizationOptions options)
         {
             if (app == null)
@@ -29,6 +43,12 @@ namespace Microsoft.Owin.Security.Authorization.Infrastructure
             return app.Use(typeof(ResourceAuthorizationMiddleware), options);
         }
 
+        /// <summary>
+        /// Adds authorization services to the specified <see cref="IAppBuilder" />. 
+        /// </summary>
+        /// <param name="app">The <see cref="IAppBuilder" /> to add services to.</param>
+        /// <param name="configure">An action delegate to configure the provided <see cref="AuthorizationOptions"/>.</param>
+        /// <returns>The <see cref="IAppBuilder"/> so that additional calls can be chained.</returns>
         public static IAppBuilder UseAuthorization(this IAppBuilder app, Action<AuthorizationOptions> configure)
         {
             if (app == null)
