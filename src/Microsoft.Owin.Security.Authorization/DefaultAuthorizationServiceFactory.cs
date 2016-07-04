17fff2b1d1c1c21f8a8ca1c3ce12bd8fd61e561a
David Parks <DavidParks8@outlook.com>
2016-07-04 11:51:00 -0500

    Added comments for authorization service factory for #46

--- src/Microsoft.Owin.Security.Authorization/DefaultAuthorizationServiceFactory.cs
+++ src/Microsoft.Owin.Security.Authorization/DefaultAuthorizationServiceFactory.cs
@@ -4,8 +4,17 @@ using Microsoft.Owin.Logging;
 
 namespace Microsoft.Owin.Security.Authorization
 {
+    /// <summary>
+    /// The default implementation of <see cref="IAuthorizationServiceFactory"/>.
+    /// </summary>
     public class DefaultAuthorizationServiceFactory : IAuthorizationServiceFactory
     {
+        /// <summary>
+        /// Creates a new <see cref="IAuthorizationService"/>.
+        /// </summary>
+        /// <param name="policyProvider">The <see cref="IAuthorizationPolicyProvider"/> for providing policies.</param>
+        /// <param name="authorizationHandlers">A set <see cref="IAuthorizationHandler"/>s for evaluating authorization.</param>
+        /// <param name="loggerFactory">An <see cref="ILoggerFactory"/> for logging.</param>
         public IAuthorizationService Create(
             IAuthorizationPolicyProvider policyProvider,
             IEnumerable<IAuthorizationHandler> authorizationHandlers,
