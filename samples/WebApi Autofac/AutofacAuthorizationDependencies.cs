66860527c41a873066d0030574d047dab964c04e
David Parks <DavidParks8@outlook.com>
2016-10-10 18:13:58 -0500

    Added a new constructor to default authorization service.  Altered one of the samples for #51

---
+++ samples/WebApi Autofac/AutofacAuthorizationDependencies.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Autofac;
+using Microsoft.Owin.Logging;
+using Microsoft.Owin.Security.Authorization;
+// ReSharper disable ValueParameterNotUsed
+
+namespace WebApi_Autofac
+{
+    public class AutofacAuthorizationDependencies : AuthorizationDependencies
+    {
+        private readonly IComponentContext _resolver;
+
+        public AutofacAuthorizationDependencies(IComponentContext resolver)
+        {
+            if (resolver == null)
+            {
+                throw new ArgumentNullException(nameof(resolver));
+            }
+
+            _resolver = resolver;
+        }
+
+        public override IAuthorizationService Service
+        {
+            get { return _resolver.Resolve<IAuthorizationService>(); }
+            set { DisallowSetMethod(); }
+        }
+
+        public override ILoggerFactory LoggerFactory
+        {
+            get { return _resolver.Resolve<ILoggerFactory>(); }
+            set { DisallowSetMethod(); }
+        }
+
+        public override IAuthorizationPolicyProvider PolicyProvider
+        {
+            get { return _resolver.Resolve<IAuthorizationPolicyProvider>(); }
+            set { DisallowSetMethod(); }
+        }
+
+        private static void DisallowSetMethod()
+        {
+            throw new InvalidOperationException("Set is not a valid operation.  Instead, register the component for resolution.");
+        }
+    }
+}
--- samples/WebApi Autofac/Startup.cs
+++ samples/WebApi Autofac/Startup.cs
@@ -26,25 +26,36 @@ namespace WebApi_Autofac
             var config = new HttpConfiguration();
             WebApiConfig.Register(config);
 
+            var options = new AuthorizationOptions();
+            options.AddPolicy(ExampleConstants.EmployeeNumber2Policy, policyBuilder =>
+            {
+                policyBuilder.RequireClaim(ExampleConstants.EmployeeClaimType, "2");
+            });
+
             var builder = new ContainerBuilder();
             builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
-            builder.RegisterType<AuthorizationDependencies>().AsImplementedInterfaces();
-
+            builder.Register(resolver => options)
+                .As<AuthorizationOptions>().ExternallyOwned();
+            builder.Register(resolver => app.GetLoggerFactory())
+                .As<ILoggerFactory>();
+            builder.Register(resolver => resolver.Resolve<ILoggerFactory>().Create("default"))
+                .As<ILogger>();
+            builder.RegisterType<DefaultAuthorizationPolicyProvider>()
+                .As<IAuthorizationPolicyProvider>();
+            builder.Register(resolver => new DefaultAuthorizationService(
+                resolver.Resolve<IAuthorizationPolicyProvider>(),
+                new[] {new PassThroughAuthorizationHandler()}))
+                .As<IAuthorizationService>();
+            
             var container = builder.Build();
-            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
 
+            options.Dependencies = new AutofacAuthorizationDependencies(container);
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+            
             app.UseAutofacMiddleware(container);
             app.UseAutofacWebApi(config);
 
-            app.UseAuthorization(options =>
-            {
-                options.AddPolicy(ExampleConstants.EmployeeNumber2Policy, policyBuilder =>
-                {
-                    policyBuilder.RequireClaim(ExampleConstants.EmployeeClaimType, "2");
-                });
-
-                options.DependenciesFactory = new AutofacAuthorizationDependenciesFactory();
-            });
+            app.UseAuthorization(options);
 
             app.UseWebApi(config);
         }
