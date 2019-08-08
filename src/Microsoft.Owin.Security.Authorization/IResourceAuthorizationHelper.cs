065e6d0621d790000d3811821ed43c40fc607344
lavignep007 <53264923+lavignep007@users.noreply.github.com>
2019-08-08 12:37:59 -0400

    Update IResourceAuthorizationHelper.cs

--- src/Microsoft.Owin.Security.Authorization/IResourceAuthorizationHelper.cs
+++ src/Microsoft.Owin.Security.Authorization/IResourceAuthorizationHelper.cs
@@ -14,10 +14,11 @@ namespace Microsoft.Owin.Security.Authorization
         /// <param name="controller">The controller from which <see cref="AuthorizationOptions"/> may be obtained.</param>
         /// <param name="user">The user to evaluate the authorize data against.</param>
         /// <param name="authorizeAttribute">The <see cref="IAuthorizeData"/> to evaluate.</param>
+        /// <param name="resource">The resource to evaluate the authorize data against.</param>
         /// <returns>
         /// A flag indicating whether authorization has succeeded.
         /// This value is <value>true</value> when the <paramref name="user"/> fulfills the <paramref name="authorizeAttribute"/>; otherwise <value>false</value>.
         /// </returns>
-        Task<bool> IsAuthorizedAsync(IAuthorizationController controller, ClaimsPrincipal user, IAuthorizeData authorizeAttribute);
+        Task<bool> IsAuthorizedAsync(IAuthorizationController controller, ClaimsPrincipal user, IAuthorizeData authorizeAttribute, object resource = null);
     }
 }
